42ee11651d6daa1ef1a8d817450e3a616eb775a9
Anatolii Samoilenko <samoilenko.nwff@gmail.com>
2024-03-18 18:05:52 +0700

    fix 2

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ async def send_message():
     return {"success": True, "message": "Message sent to Telegram group."}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
